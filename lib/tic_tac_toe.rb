6765cff61a2b2a7dcd5c25c16542356b92a60855
Helen Liutongco <bellpickle@gmail.com>
2017-12-19 04:06:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,4 +29,8 @@ class TicTacToe
   def move(index, current_player="X")
     @board[index] = current_player
   end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
 end
