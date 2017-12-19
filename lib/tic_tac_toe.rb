d044c8e3835744ffec2a177f0313c066da35b268
Helen Liutongco <bellpickle@gmail.com>
2017-12-19 04:13:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,4 +61,13 @@ class TicTacToe
       turn
     end
   end
+
+  def won?
+    WIN_COMBINATIONS.each do |combination|
+      if (@board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X") || (@board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O")
+        return combination
+      end
+    end
+    false
+  end
 end
